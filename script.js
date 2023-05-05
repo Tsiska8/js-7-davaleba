4d177809002e15f205f74a0919b6f1a58d5d4cb4
Ketevan <qeto71@gmail.com>
2023-05-05 11:25:44 +0400

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var m = [1, 2, -9, -56, 34, 77, -90 -11, -1, -2, -5, -9];
 var index = 0;
 
 while(index < m.length) {
-     if(m < 0) {
+     if(m[index] < 0) {
         document.write(m[index] + "<br>");
     }
     index++;
